79f121b14f63cc27a4f329671a359f044feea005
jused <jusedadinsi@gmail.com>
2026-02-09 15:28:49 +0100

    Maj Dockerfiles

--- src/main.ts
+++ src/main.ts
@@ -13,6 +13,6 @@ async function bootstrap() {
     const document = SwaggerModule.createDocument(app, config);
     SwaggerModule.setup('/', app, document); // swagger à la racine '/'
 
-    await app.listen(8081);
+    await app.listen(3000);
 }
 bootstrap();
