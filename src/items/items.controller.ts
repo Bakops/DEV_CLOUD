15fd21f48807fba65f8fadd4dd6ccd479e66d26a
Bakou Toure <bakoutoubo.toure@atos.net>
2026-02-12 15:39:34 +0100

    Maj rroute erreur

--- src/items/items.controller.ts
+++ src/items/items.controller.ts
@@ -31,7 +31,7 @@ export class ItemsController {
         return this.service.remove(+id);
     }
 
-    @Get('erreur')
+    @Get('LOL')
     triggerError() {
         throw new Error('BOMBOCLAT! il y a une erreur.');
     }
